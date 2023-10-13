78c29a1894be6b3cd504c8992330db8ba78f6950
Wyatt Raphael <wraphael@securityinnovation.com>
2023-10-13 11:01:08 -0700

    Fix keymap.c and rules.mk

--- keyboards/splitkb/kyria/keymaps/rapwya/keymap.c
+++ keyboards/splitkb/kyria/keymaps/rapwya/keymap.c
@@ -23,6 +23,11 @@ enum layers {
     GAME
 };
 
+// Aliases for readability
+#define CTL_ESC  MT(MOD_LCTL, KC_ESC)
+#define CTL_QUOT MT(MOD_RCTL, KC_QUOTE)
+#define ALT_ENT  MT(MOD_LALT, KC_ENT)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * Base Layer: QWERTY
@@ -42,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q, KC_W, KC_E,  KC_R,    KC_T,                                         KC_Y,           KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
       CTL_ESC, KC_A, KC_S, KC_D,  KC_F,    KC_G,                                         KC_H,           KC_J,    KC_K,    KC_L,   KC_SCLN, CTL_QUOT,
       KC_LSFT, KC_Z, KC_X, KC_C,  KC_V,    KC_B,    KC_LBRC, KC_CAPS,    MO(3), KC_RBRC, KC_N,           KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                           MO(4), KC_LGUI, ALT_ENT, KC_SPC,  MO(1),      MO(2), KC_SPC,  RALT_T(KC_ENT), KC_RGUI, KC_APP
+                           DF(4), KC_LGUI, ALT_ENT, KC_SPC,  MO(1),      MO(2), KC_SPC,  RALT_T(KC_ENT), KC_RGUI, KC_APP
     ),
     /*
      * Nav Layer: Media, navigation
